287475955d9e70530d16d52036f65207498745af
galihprad <galih.p.w.j@gmail.com>
2022-08-24 11:30:18 +0700

    try API 5

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const { Telegraf } = require("telegraf");
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf("5793524297:AAHrdBqsfFAZU95RScWVRghlslZIu9eW4Ow");
 bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on("sticker", (ctx) => ctx.reply("👍"));
